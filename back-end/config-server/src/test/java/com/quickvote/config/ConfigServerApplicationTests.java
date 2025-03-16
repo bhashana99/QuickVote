5b4afaddf97d55ef216e5137c0d16293e272a34b
bhashana99 <bhashanachamodya99@gmail.com>
2025-03-16 18:45:51 +0530

    first commit

---
+++ back-end/config-server/src/test/java/com/quickvote/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickvote.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back-end/election-service/src/test/java/com/quickvote/election/ElectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickvote.election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
